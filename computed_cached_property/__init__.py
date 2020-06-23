30672ce4400ad10a56578cf6354bf1f7ef4c2df4
Lisonge <lisonge@users.noreply.github.com>
2020-06-23 23:40:02 +0800

    update

---
+++ computed_cached_property/__init__.py
@@ -0,0 +1,10 @@
+'''
+@Date: 2020-06-23 22:42:05
+@LastEditors: code
+@Author: code
+@LastEditTime: 2020-06-23 22:45:48
+'''
+
+from .base import computed_cached_property
+
+name = 'computed_cached_property'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 @Date: 2020-06-19 22:55:45
 @LastEditors: code
 @Author: code
-@LastEditTime: 2020-06-23 21:24:40
+@LastEditTime: 2020-06-23 23:02:15
 '''
 import setuptools
 
@@ -12,15 +12,15 @@ with open('./README.md', 'r') as fp:
 setuptools.setup(
     name='computed-cached-property',
     license='MIT',
-    version='0.0.1',
+    version='0.0.3',
     author='Li Songe',
     author_email='i@songe.li',
     description='''A decorator for caching computed properties in classes. like Vue's Computed-Properties''',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/lisonge/computed-cached-property',
-    # packages=setuptools.find_packages(),
-    py_modules=['computed_cached_property'],
+    packages=setuptools.find_packages(),
+    # py_modules=['computed_cached_property'],
     keywords='cached-property',
     classifiers=[
         'Programming Language :: Python :: 3',
